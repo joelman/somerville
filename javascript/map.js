6504d7be515faa7a2e84e27cd09648695c17c04b
jgwynn01 <joel.gwynn@tufts.edu>
2021-01-14 17:54:08 -0500

    added pricing to popup

--- javascript/map.js
+++ javascript/map.js
@@ -9,6 +9,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: token
 }).addTo(mymap);
 
+var red = 8388608;
+var green = 800;
+
+var minVal = 900;
+var maxVal = 1838600;
+
 function lookup(e) {
 
 //    console.log(e);
@@ -50,37 +56,97 @@ const draw = async() => {
             lat: p[2],
             lon: p[3]
         };
-        addresses.push(a);
-
-        var circle = L.circle([a.lat, a.lon], {
+		
+		
+		var circle = L.circle([a.lat, a.lon], {
         color: 'blue',
         fillColor: 'blue',
         fillOpacity: 1,
-        radius: 1
+        radius: 1,
+		lat: a.lat,
+		lon: a.lon
         });
-
-        circle.bindPopup(`${a.number} ${a.street}`).openPopup();
-	circle.on('click', lookup);
+		
+        // circle.bindPopup(`${a.number} ${a.street}`).openPopup();
+		circle.bindPopup(hi).openPopup();
+		circle.on('click', lookup);
 
         circle.addTo(mymap);
+		
+        addresses.push(a);
+        
     }
 
     log(`loaded ${addresses.length} addresses.`)
 }
 
+function hi(e) {
+	console.log(e);
+	var o = e.options;
+	var parcels = properties.filter(x => x.lat == x.lat && x.lon == o.lon)
+	console.log(parcels);
+	
+	var data = [`${parcels[0].HOUSE_NO} ${parcels[0].STREET}`];
+	if(parcels[0].UNIT) {
+		data.push(`${parcels.length} Units`);
+	}
+	
+	for(var i = 0; i < parcels.length; i++) {
+		var parcel = parcels[i];
+		var line = '';
+		if(parcel.UNIT) {
+			line = `Unit: ${parcel.UNIT} `;
+		}
+		
+		var price = parseInt(parcel.SALE_PRICE);
+		var pf = price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+		var val = parseInt(parcel.PARCEL_VAL);
+		var vf = val.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+		line += `Price: ${pf} Value: ${vf}`;
+		
+		data.push(line);
+	}
+	o.color = 'red'
+	return data.join('<br />')
+	
+}
+
 const load = async() => {
 
     var length = propertyRows.length;
-
+	var plotted = [];
+	
     for (var i = 0; i < length; i++) {
         var x = propertyRows[i].split('\t')
 
             var plot = {};
         for (var f = 0; f < fields.length; f++) {
-            plot[fields[f]] = x[f];
+            plot[fields[f].replace(' ', '_')] = x[f];
         }
 
         properties.push(plot);
+		/*
+		if(plot.lat == "None")
+			continue;
+		
+		var key = `${plot.lat},${plot.lon}`
+		if(plotted.includes(key))
+			continue;
+		
+		var circle = L.circle([plot.lat, plot.lon], {
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 1,
+        radius: 1
+        });
+		
+        circle.bindPopup(`${plot.HOUSE_NO} ${plot.STREET}`).openPopup();
+		circle.on('click', lookup);
+
+        circle.addTo(mymap);
+		
+		plotted.push(key);
+		*/
     }
     log(`loaded ${properties.length} properties.`)
 }
