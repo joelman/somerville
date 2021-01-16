4106c16865f03636bbc71854e6a90a5e2bd4aba8
Joel Gwynn <joelman@gmail.com>
2021-01-16 15:49:20 -0500

    added legend

--- javascript/map.js
+++ javascript/map.js
@@ -9,14 +9,119 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: token
 }).addTo(mymap);
 
-var red = 8388608;
-var green = 800;
+var markers = [];
+
+var scale = 1000000;
+var step = 100000;
+
+// https://colordesigner.io/gradient-generator
+var colors = ['#0000ff', '#9900e2', '#cf00c1', '#f100a0', '#ff0080', '#ff0063', '#ff004a', '#ff3133', '#ff601c', '#ff8000']
+
+function initControls() {
+
+    var table = document.getElementById('legend')
+
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    let text = document.createTextNode("Price");
+    let th = document.createElement("th");
+    th.appendChild(text);
+    th.colSpan = 2;
+    row.appendChild(th);
+
+    for(let i = 0; i < scale; i += step) {
+	let row = table.insertRow();
+	let c1 = document.createElement('td');
+	c1.bgColor = colors[(i/step)];
+
+	let check = document.createElement("INPUT");
+	check.setAttribute("type", "checkbox");
+	check.setAttribute('id', `index_${(i/step)}`);
+	check.setAttribute('class', 'rangeCheck');
+//	check.checked = true;
+	check.addEventListener("click", draw);
+	c1.appendChild(check);
+	row.appendChild(c1);
+
+	let c2 = document.createElement('td')
+
+	let text = '';
+	if(i == scale - step) {
+	    text = `${i.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} and over`;
+	} else {
+	    var from = i.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+	    var to = (i + step).toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+	    text = `${from} to ${to}`;
+	}
+	
+	let t = document.createTextNode(text);
+	c2.appendChild(t);
+	row.appendChild(c2);
+    }
+}
 
 var minPrice = 100000000;
 var maxPrice = 0;
 
 var minValue = 100000000;
-var maxValue = 0;
+var maxValue = 0;   
+
+
+var addresses = [];
+var properties = [];
+
+const draw = async() => {
+
+    for(var i = 0; i < markers.length; i++) {
+	mymap.removeLayer(markers[i]);
+    }
+
+    markers = [];
+    
+    let checks = document.getElementsByClassName('rangeCheck');
+    let selected = [];
+    for(var i = 0; i < checks.length; i++) {
+	selected.push(checks[i].checked);
+    }
+
+//    console.log(selected);
+    
+    let length = addresses.length;
+    for (var i = 0; i < length; i++) {
+	
+	var address = addresses[i]
+
+	var color = 0;
+
+	for(var c = step; c < scale; c += step) {
+	    if(address.price >= c) {
+		color++;
+	    }
+	}
+
+	if(!selected[color]) {
+	    continue;
+	}
+	
+	var circle = L.circle([address.lat, address.lon], {
+            color: colors[color],
+            fillColor: colors[color],
+            fillOpacity: 1,
+            radius: 5,
+	    lat: address.lat,
+	    lon: address.lon
+        });
+		
+        // circle.bindPopup(`${a.number} ${a.street}`).openPopup();
+	circle.bindPopup(hi).openPopup();
+	circle.on('click', lookup);
+
+	markers.push(circle);
+        circle.addTo(mymap);
+    }
+
+    log(`loaded ${addresses.length} addresses.`)
+}
 
 function lookup(e) {
 
@@ -44,71 +149,6 @@ function lookup(e) {
 }
 
 mymap.on('click', lookup);
-
-var addresses = [];
-var properties = [];
-
-const draw = async() => {
-
-    var length = addressRows.length;
-    for (var i = 0; i < length; i++) {
-        var p = addressRows[i].split('\t');
-        var a = {
-	    price: parseInt(p[0]),
-	    value: parseInt(p[1]),
-            number: p[2],
-            street: p[3],
-            lat: parseFloat(p[4]),
-            lon: parseFloat(p[5])
-        };
-
-	if(a.price > maxPrice) {
-	    maxPrice = a.price;
-	}
-
-	if(a.value > maxValue) {
-	    maxValue = a.value;
-	}
-
-	if(a.price < minPrice && a.price > 0) {
-	    minPrice = a.price;
-	}
-
-	if(a.value < minValue && a.value > 0) {
-	    minValue = a.value;
-	}
-	
-        addresses.push(a);
-    }	
-
-	log(`minPrice: ${minPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
-	log(`maxPrice: ${maxPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
-
-	log(`minValue: ${minValue.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
-	log(`maxValue: ${maxValue.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
-	
-    for (var i = 0; i < length; i++) {
-
-	var address = addresses[i]
-	
-	var circle = L.circle([address.lat, address.lon], {
-            color: 'blue',
-            fillColor: 'blue',
-            fillOpacity: 1,
-            radius: 1,
-	    lat: address.lat,
-	    lon: address.lon
-        });
-		
-        // circle.bindPopup(`${a.number} ${a.street}`).openPopup();
-	circle.bindPopup(hi).openPopup();
-	circle.on('click', lookup);
-
-        circle.addTo(mymap);
-    }
-
-    log(`loaded ${addresses.length} addresses.`)
-}
 
 function hi(e) {
 	console.log(e);
@@ -142,9 +182,48 @@ function hi(e) {
 }
 
 const load = async() => {
+    
+    let length = addressRows.length;
+    for (var i = 0; i < length; i++) {
+        var p = addressRows[i].split('\t');
+        var a = {
+	    price: parseInt(p[0]),
+	    value: parseInt(p[1]),
+            number: p[2],
+            street: p[3],
+            lat: parseFloat(p[4]),
+            lon: parseFloat(p[5])
+        };
 
-    var length = propertyRows.length;
-	var plotted = [];
+	if(a.price > maxPrice) {
+	    maxPrice = a.price;
+	}
+
+	if(a.value > maxValue) {
+	    maxValue = a.value;
+	}
+
+	if(a.price < minPrice && a.price > 0) {
+	    minPrice = a.price;
+	}
+
+	if(a.value < minValue && a.value > 0) {
+	    minValue = a.value;
+	}
+	
+        addresses.push(a);
+    }	
+
+    draw();
+    
+    log(`minPrice: ${minPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
+    log(`maxPrice: ${maxPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
+
+    log(`minValue: ${minValue.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
+    log(`maxValue: ${maxValue.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
+
+    length = propertyRows.length;
+    var plotted = [];
 	
     for (var i = 0; i < length; i++) {
         var x = propertyRows[i].split('\t')
@@ -188,5 +267,5 @@ function log(message) {
 
 }
 
-draw();
+initControls();
 load();
